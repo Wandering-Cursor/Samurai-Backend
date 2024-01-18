d386c0631dc10bb0e9d10b80fd25a5b5fb3f07bf
Pavlo Pohorieltsev <667strets@gmail.com>
2024-01-18 18:39:48 +0200

    feat: Change Task Status endpoint

--- DiplomaPulse/students/serializers/tasks/task_details.py
+++ DiplomaPulse/students/serializers/tasks/task_details.py
@@ -1,8 +1,7 @@
-from rest_framework import serializers, status
+from rest_framework import serializers
 
-from students.models.task import UserTask
 from students.serializers.base import AccountSerializerMixIn
-from students.serializers.tasks.task import TaskSerializer
+from students.serializers.tasks.task import TaskFinderMixin, TaskSerializer
 
 
 class TaskDetailsInputSerializer(serializers.Serializer):
@@ -13,26 +12,6 @@ class TaskDetailsOutputSerializer(TaskSerializer):
 	pass
 
 
-class GetTaskDetailsSerializer(AccountSerializerMixIn, TaskDetailsInputSerializer):
-	task: UserTask = None
-
-	def validate_task_id(self, task_id):
-		task_not_found = serializers.ValidationError(
-			detail=f"Task not found with {task_id=}",
-			code=status.HTTP_404_NOT_FOUND,
-		)
-
-		qs = UserTask.objects.filter(id=task_id)
-		task = qs.first()
-		if not task:
-			raise task_not_found
-
-		if not task.user_projects.filter(student=self.student_entity).exists():
-			raise task_not_found
-
-		self.task = task
-
-		return task.id
-
+class GetTaskDetailsSerializer(AccountSerializerMixIn, TaskDetailsInputSerializer, TaskFinderMixin):
 	def create(self):
 		return TaskDetailsOutputSerializer(self.task)
---
+++ DiplomaPulse/students/views/tasks/task_update.py
@@ -0,0 +1,45 @@
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from students.serializers.tasks.task import TaskNotFound, TaskSerializer
+from students.serializers.tasks.task_update import (
+	UpdateTaskForbiddenSerializer,
+	UpdateTaskInputSerializer,
+	UpdateTaskSerializer,
+)
+from students.views.base import StudentView
+
+
+class UpdateTask(StudentView):
+	serializer_class = UpdateTaskSerializer
+
+	@swagger_auto_schema(
+		operation_summary="Returns details about the task",
+		request_body=UpdateTaskInputSerializer,
+		responses={
+			status.HTTP_200_OK: TaskSerializer,
+			status.HTTP_403_FORBIDDEN: UpdateTaskForbiddenSerializer,
+			status.HTTP_404_NOT_FOUND: TaskNotFound,
+		},
+		tags=["students", "tasks"],
+	)
+	def patch(self, request: Request):
+		try:
+			serializer = self.serializer_class(
+				data={
+					"account_uuid": request.user.id,
+					"task_id": request.data.get("task_id", None),
+					"state": request.data.get("state", None),
+				}
+			)
+			serializer.is_valid(raise_exception=True)
+
+			task = serializer.create(serializer.validated_data)
+		except Exception as e:
+			arg: UpdateTaskForbiddenSerializer | TaskNotFound = e.args[0]
+			arg.is_valid(raise_exception=True)
+			return Response(data=arg.data, status=arg.data["code"])
+
+		return Response(data=TaskSerializer(instance=task).data)
