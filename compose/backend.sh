f79778f05df4fac3fb2cfa2e15ef49f0f263b9d3
Pavlo Pohorieltsev <49622129+makisukurisu@users.noreply.github.com>
2024-03-16 19:16:32 +0200

    fix: Separate stop-containers step; Add migrations support (#33)

--- compose/backend.sh
+++ compose/backend.sh
@@ -4,6 +4,14 @@ set -e -x
 echo "Activating virtual environment"
 source .venv/bin/activate
 
+echo "Running migrations"
+cd /app/src/samurai_backend
+alembic upgrade head
+echo "Migrations complete"
+
+# Return to root directory
+cd /app
+
 echo "Starting backend server"
 uvicorn samurai_backend.app:app --proxy-headers --host 0.0.0.0 --port 8000
 echo "Backend server exited"
