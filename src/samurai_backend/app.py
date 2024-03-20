13cbaeea1fe31360abfe8b79dc9a65394a85d482
Pavlo Pohorieltsev <49622129+makisukurisu@users.noreply.github.com>
2024-03-20 19:14:37 +0200

    chore: Fix add nonce to redoc

--- src/samurai_backend/app.py
+++ src/samurai_backend/app.py
@@ -119,10 +119,54 @@ async def swagger_ui_redirect() -> HTMLResponse:
     include_in_schema=False,
 )
 async def custom_redoc_html() -> str:
-    return get_redoc_html(
-        openapi_url=app.openapi_url,
-        title=app.title + " - ReDoc",
-        redoc_js_url="/static/redoc.standalone.js",
-        redoc_favicon_url="/static/favicon.png",
-        with_google_fonts=False,
-    )
+    """
+    Generate and return the HTML response that loads ReDoc for the alternative
+    API docs (normally served at `/redoc`).
+
+    You would only call this function yourself if you needed to override some parts,
+    for example the URLs to use to load ReDoc's JavaScript and CSS.
+
+    Read more about it in the
+    [FastAPI docs for Custom Docs UI Static Assets (Self-Hosting)](https://fastapi.tiangolo.com/how-to/custom-docs-ui-assets/).
+    """
+    openapi_url = app.openapi_url
+    title = app.title + " - ReDoc"
+    redoc_js_url = "/static/redoc.standalone.js"
+    redoc_favicon_url = "/static/favicon.png"
+    with_google_fonts = False
+
+    html = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title>{title}</title>
+    <!-- needed for adaptive design -->
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    """
+    if with_google_fonts:
+        html += """
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Roboto:300,400,700" rel="stylesheet">
+    """
+    html += f"""
+    <link rel="shortcut icon" href="{redoc_favicon_url}">
+    <!--
+    ReDoc doesn't change outer page styles
+    -->
+    <style>
+      body {{
+        margin: 0;
+        padding: 0;
+      }}
+    </style>
+    </head>
+    <body>
+    <noscript>
+        ReDoc requires Javascript to function. Please enable it to browse the documentation.
+    </noscript>
+    <redoc spec-url="{openapi_url}"></redoc>
+    <script src="{redoc_js_url}" nonce={settings.script_nonce}> </script>
+    </body>
+    </html>
+    """
+    return HTMLResponse(html)
