2995983aa65c0e5d0fb9919222f959593f01aba6
Pavlo Pohorieltsev <49622129+makisukurisu@users.noreply.github.com>
2024-03-20 17:53:09 +0200

    Add cors regex

--- src/samurai_backend/middleware.py
+++ src/samurai_backend/middleware.py
@@ -8,6 +8,7 @@ def add_cors_middleware(app: FastAPI) -> None:
     app.add_middleware(
         CORSMiddleware,
         allow_origins=security_settings.cors_allow_origins,
+        allow_origin_regex=security_settings.cors_allow_origin_regex,
         allow_credentials=security_settings.cors_allow_credentials,
         allow_methods=security_settings.cors_allow_methods,
         allow_headers=security_settings.cors_allow_headers,
