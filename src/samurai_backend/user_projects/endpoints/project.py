914231ee6c9c0e132cdcc30abea742a4d54c5cb8
Pavlo Pohorieltsev <49622129+makisukurisu@users.noreply.github.com>
2024-04-03 19:42:00 +0300

    add sample points

--- src/samurai_backend/user_projects/endpoints/project.py
+++ src/samurai_backend/user_projects/endpoints/project.py
@@ -1,12 +1,15 @@
 from typing import Annotated
 
+import pydantic
 from fastapi import Depends
 
 from samurai_backend.dependencies import database_session, database_session_type
 from samurai_backend.enums import Permissions
+from samurai_backend.models.user_projects.project import UserProjectModel
 from samurai_backend.organization.get import user_project as project_get
 from samurai_backend.organization.schemas.user_project import (
     ProjectSearchInput,
+    UserProjectRepresentation,
     UserProjectSearchOutput,
 )
 from samurai_backend.user_projects.router import user_projects_router
@@ -23,9 +26,58 @@ async def get_projects(
     search: Annotated[ProjectSearchInput, Depends()],
 ) -> UserProjectSearchOutput:
     """
-    Get all available projects for the student.
+    Get all available projects by specifying the search criteria.
     """
     return project_get.search_projects(
         session=session,
         search_input=search,
+    )
+
+
+@user_projects_router.get(
+    "/projects/{project_id}",
+    dependencies=[
+        Permissions.PROJECTS_READ.as_security,
+    ],
+)
+async def get_project(
+    session: Annotated[database_session_type, Depends(database_session)],
+    project_id: pydantic.UUID4,
+) -> UserProjectRepresentation:
+    """
+    Get a specific project for the student.
+    """
+    return UserProjectRepresentation.model_validate(
+        project_get.get_project_by_id(
+            session=session,
+            project_id=project_id,
+        ),
+        from_attributes=True,
+    )
+
+
+@user_projects_router.post(
+    "/projects",
+    dependencies=[
+        Permissions.PROJECTS_CREATE.as_security,
+    ],
+)
+async def create_project(
+    session: Annotated[database_session_type, Depends(database_session)],
+    project: UserProjectRepresentation,
+) -> UserProjectRepresentation:
+    """
+    Create a new project for the student.
+    """
+    project_entity = UserProjectModel.model_validate(
+        project,
+        from_attributes=True,
+    )
+
+    session.add(project_entity)
+    session.commit()
+
+    return UserProjectRepresentation.model_validate(
+        project_entity,
+        from_attributes=True,
     )
