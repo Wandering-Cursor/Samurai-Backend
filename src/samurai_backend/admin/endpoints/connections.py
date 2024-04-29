f9d3acf9739dbdb377d0707fdb69cbf5c7b9d5fb
Pavlo Pohorieltsev <49622129+makisukurisu@users.noreply.github.com>
2024-04-29 17:21:52 +0300

    feat: Editing connections

--- src/samurai_backend/admin/endpoints/connections.py
+++ src/samurai_backend/admin/endpoints/connections.py
@@ -4,6 +4,7 @@ from fastapi import Depends
 
 from samurai_backend.admin.get import connections as connections_get
 from samurai_backend.admin.router import admin_router
+from samurai_backend.admin.schemas import connections as connections_schemas
 from samurai_backend.core.operations import store_entity
 from samurai_backend.dependencies import database_session, database_session_type
 from samurai_backend.models.account.connection import (
@@ -15,22 +16,27 @@ from samurai_backend.models.account.connection import (
 
 @admin_router.get(
     "/connections",
-    description="Get all connections.",
 )
 async def get_connections(
     db: Annotated[database_session_type, Depends(database_session)],
-) -> list[ConnectionBase]:
-    return connections_get.get_connections(db=db)
+    search: Annotated[connections_schemas.ConnectionsFilter, Depends()],
+) -> connections_schemas.ConnectionsPaginatedResponse:
+    "Execute a search among connections"
+    return await connections_get.get_connections(
+        session=db,
+        search=search,
+    )
 
 
 @admin_router.post(
     "/connections",
-    description="Create a permission.",
 )
 async def create_connection(
     db: Annotated[database_session_type, Depends(database_session)],
     connection: ConnectionCreate,
 ) -> ConnectionBase:
+    """Create a new connection"""
+
     return ConnectionBase.model_validate(
         store_entity(
             db=db,
--- src/samurai_backend/admin/get/connections.py
+++ src/samurai_backend/admin/get/connections.py
@@ -4,20 +4,54 @@ from typing import TYPE_CHECKING
 
 from sqlmodel import select
 
+from samurai_backend.admin.schemas.connections import ConnectionsPaginatedResponse
 from samurai_backend.models.account.connection import ConnectionModel
+from samurai_backend.utils.get_count import get_count
 
 if TYPE_CHECKING:
     import pydantic
     from sqlmodel import Session
 
+    from samurai_backend.admin.schemas.connections import ConnectionsFilter
 
-def get_connections(
-    db: Session,
-) -> list[ConnectionModel]:
+
+async def get_connections(
+    session: Session,
+    search: ConnectionsFilter,
+) -> ConnectionsPaginatedResponse:
     query = select(ConnectionModel).order_by(
         ConnectionModel.updated_at.desc(),
     )
-    return db.exec(query).all()
+
+    if search.connection_id:
+        query = query.filter(
+            ConnectionModel.connection_id == search.connection_id,
+        )
+    if search.group_id:
+        query = query.filter(
+            ConnectionModel.group_id == search.group_id,
+        )
+    if search.faculty_id:
+        query = query.filter(
+            ConnectionModel.faculty_id == search.faculty_id,
+        )
+    if search.department_id:
+        query = query.filter(
+            ConnectionModel.department_id == search.department_id,
+        )
+
+    total = get_count(session, query)
+    query = query.offset(search.offset).limit(search.page_size)
+    content = session.exec(query)
+
+    return ConnectionsPaginatedResponse(
+        meta=ConnectionsPaginatedResponse.construct_meta(
+            total=total,
+            page=search.page,
+            page_size=search.page_size,
+        ),
+        content=content,
+    )
 
 
 def get_connection(
