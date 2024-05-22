ba10d2148c5a998e634d3b620135a53c23432e76
Pavlo Pohorieltsev <49622129+makisukurisu@users.noreply.github.com>
2024-05-22 19:56:49 +0300

    fix pagination count

--- src/samurai_backend/utils/get_count.py
+++ src/samurai_backend/utils/get_count.py
@@ -1,17 +1,9 @@
-from sqlalchemy import func
-from sqlmodel import Session
+from sqlmodel import Session, func
 from sqlmodel.sql.expression import SelectOfScalar
 
 
-def get_count(
-    session: Session,
-    q: SelectOfScalar,
-    join: bool = True,
-) -> int:
-    count_q = q.with_only_columns(func.count()).order_by(None)
-    if join:
-        count_q = count_q.select_from(*q.froms)
-
+def get_count(session: Session, q: SelectOfScalar) -> int:
+    count_q = q.with_only_columns(func.count()).order_by(None).select_from(q.get_final_froms()[0])
     iterator = session.exec(count_q)
     for count in iterator:
         return count
