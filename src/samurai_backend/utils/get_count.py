24ece3d38fe41b672f3cba23ae2ac6dce8b8d207
Pavlo Pohorieltsev <49622129+makisukurisu@users.noreply.github.com>
2024-05-22 21:47:56 +0300

    fix: Correct count not to double join

--- src/samurai_backend/utils/get_count.py
+++ src/samurai_backend/utils/get_count.py
@@ -2,8 +2,11 @@ from sqlmodel import Session, func
 from sqlmodel.sql.expression import SelectOfScalar
 
 
-def get_count(session: Session, q: SelectOfScalar) -> int:
-    count_q = q.with_only_columns(func.count()).order_by(None).select_from(q.get_final_froms()[0])
+def get_count(
+    session: Session,
+    q: SelectOfScalar,
+) -> int:
+    count_q = q.with_only_columns(func.count()).order_by(None)
     iterator = session.exec(count_q)
     for count in iterator:
         return count
