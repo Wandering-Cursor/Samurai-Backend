261b04d22239e75ac2c066fcd4a7a04a86bf3db1
Pavlo Pohorieltsev <49622129+makisukurisu@users.noreply.github.com>
2024-05-23 17:50:45 +0300

    fix: Add use argon2 as primary password hashing method

--- src/samurai_backend/utils/pwd_context.py
+++ src/samurai_backend/utils/pwd_context.py
@@ -1,6 +1,9 @@
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(
-    schemes=["bcrypt"],
+    schemes=["argon2", "bcrypt"],
     deprecated="auto",
+    argon2__rounds=8,
+    argon2__memory_cost=32 * 1024,  # 32MB
+    argon2__parallelism=1,
 )
