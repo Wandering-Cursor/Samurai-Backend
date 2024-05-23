666d992df2982e320584a41c8f2716b2b700fb3c
Pavlo Pohorieltsev <49622129+makisukurisu@users.noreply.github.com>
2024-05-23 08:07:38 +0300

    fix count

--- src/samurai_backend/utils/get_count.py
+++ src/samurai_backend/utils/get_count.py
@@ -7,6 +7,8 @@ def get_count(
     q: SelectOfScalar,
 ) -> int:
     count_q = q.with_only_columns(func.count()).order_by(None)
+    if len(count_q.get_final_froms()) == 0:
+        count_q = count_q.select_from(q.get_final_froms()[0])
     iterator = session.exec(count_q)
     for count in iterator:
         return count
